f21deaa72ab98de68a1f232ecf3a13707dcb3b80
Pontus Aurdal <pontus.edvard@gmail.com>
2018-01-31 16:47:41 +0100

    Add transaction fetch

--- VueSampleApplication/src/components/Bank.vue
+++ VueSampleApplication/src/components/Bank.vue
@@ -1,21 +1,36 @@
 <template lang="pug">
 #bank(v-if="accounts.length > 0")
-  md-list.md-double-line.md-elevation-
-1    md-list-item(
+  md-list.md-double-line.md-elevation-1
+    md-list-item(
       v-for="(item, index) in accounts"
       :key="index"
+      md-expand
+      :md-expanded.sync="expanded"
+      @click="fetchTransactions(item.accountNumber)"
     )
       md-avatar.md-avatar-icon.md-accent {{ initials }}
       .md-list-item-text
         span {{ item.name }}
         span {{ item.accountNumber }}
-      md-button.md-icon-button.md-list-action
-        md-icon keyboard_arrow_down
+      
+      md-list.md-dense(slot="md-expand")
+        md-list-item(
+          v-for="(transaction, index_2) in transactions.items"
+          :key="index_2"
+        )
+          md-icon settings_ethernet
+          .md-list-item-text
+            span {{ transaction.transactionId }}
+            span {{ transaction.amount }} NOK
 </template>
 
 <script>
 export default {
   name: 'Bank',
+  data: () => ({
+    expanded: false,
+    transactions: []
+  }),
   computed: {
     accounts () {
       return this.$store.getters['Bank/accounts']
@@ -24,10 +39,28 @@ export default {
       const data = this.$store.getters['Customers/data']
       return data == null ? null : data.firstName.substr(0, 1) + data.lastName.substr(0, 1)
     }
+  },
+  methods: {
+    fetchTransactions (accountNumber) {
+      if (this.expanded)
+        return
+
+      this.$store.dispatch('Bank/transactions', accountNumber)
+        .then(list => {
+          this.transactions = list.items
+        })
+    },
+    test () {
+      alert("Fired")
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+#bank {
+  .transactions {
 
+  }
+}
 </style>
--- VueSampleApplication/src/store/modules/Bank.js
+++ VueSampleApplication/src/store/modules/Bank.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import * as t from '@/store/types'
 import {
   USER_ID,
-  ENDPOINT_ACCOUNTS
+  ENDPOINT_ACCOUNTS,
+  ENDPOINT_TRANSACTIONS
 } from '@/config'
 
 const state = {
@@ -17,12 +18,29 @@ const mutations = {
 
 const actions = {
   load ({commit}) {
-    Vue.resource(ENDPOINT_ACCOUNTS)
+    return Vue.resource(ENDPOINT_ACCOUNTS)
       .get({customerId: USER_ID})
       .then(r => {
         return r.status == 200 && !r.body.isError ? (
           commit(t.BANK_SET_ACCOUNTS, r.body.items),
           Promise.resolve()
+        ) : (
+          Promise.reject(r.body.errorMessage)
+        )
+      })
+  },
+
+  transactions ({commit}, accountNumber) {
+    return Vue.resource(ENDPOINT_TRANSACTIONS)
+      .get({
+        customerId: USER_ID,
+        accountNumber,
+        index: 0,
+        length: 10
+      })
+      .then(r => {
+        return r.status == 200 && !r.body.isError ? (
+          Promise.resolve(r.body)
         ) : (
           Promise.reject(r.body.errorMessage)
         )
