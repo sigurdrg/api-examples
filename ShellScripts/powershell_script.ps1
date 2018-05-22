9e04a1f62bdeb4eca9b6bf1720e22856e28464b9
Helland, Bjarte Konglevoll <bjarte.helland@sbanken.no>
2018-05-22 14:50:43 +0200

    update powershell script with call for specific account

--- ShellScripts/powershell_script.ps1
+++ ShellScripts/powershell_script.ps1
@@ -28,3 +28,16 @@ $authHeaders.Add("Authorization", "Bearer "+$authResponse.access_token)
 $accountUri = "https://api.sbanken.no/Bank/api/v1/Accounts"
 $response = Invoke-RestMethod -Uri $accountUri -Method GET -Headers $authHeaders
 $response
+
+
+echo "Get spesific account based on AccountId"
+$authHeaders = @{}
+$authHeaders.Add("Accept", "application/json")
+$authHeaders.Add("customerId", $userid);
+$authHeaders.Add("Authorization", "Bearer "+$authResponse.access_token)
+$accountUri = "https://api.sbanken.no/Bank/api/v1/Accounts/" +  $response.items[0].accountId
+$response = Invoke-RestMethod -Uri $accountUri -Method GET -Headers $authHeaders
+$response
+
+
+
