453a6f23af5114268a57283a0531039b3c1001aa
Yngve Høiseth <yngve@hoiseth.net>
2018-07-27 11:49:45 +0200

    Use environment variables for credentials

--- PythonSampleApplication/api_settings.py
+++ PythonSampleApplication/api_settings.py
@@ -1,6 +1,8 @@
+from os import environ
+
 # This is your social security number. The same Id which is used when you log in with BankID.
-CUSTOMERID = ""
+CUSTOMERID = environ['CUSTOMERID']
 
 # Get CLIENTID ('Applikasjonsnøkkel') and SECRET ('Passord') from https://secure.sbanken.no/Personal/ApiBeta/Info
-CLIENTID = ""
-SECRET = ""
+CLIENTID = environ['CLIENTID']
+SECRET = environ['SECRET']
